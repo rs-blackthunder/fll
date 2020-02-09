ab86c306d05eae248f20dcadbfdbcfbd6152b9b5
RishabhL <44410062+RishabhL@users.noreply.github.com>
2020-02-09 21:04:00 +0000

    Update Selecting Missions
    Missions can now be selected just through brick buttons and not through the colour sensor.

--- 2019-20/Nationals/main.py
+++ 2019-20/Nationals/main.py
@@ -43,6 +43,42 @@ missions.Mission9_2()
 
 # sys.exit()
 
+# A better way to select missions than the colour sensor that may not work in different environments.
+selecting = True
+
+while selecting:
+  while not (any(brick.buttons())):
+    wait(10)
+  if Button.UP in brick.buttons():
+    print("Mission 2")
+    missions.Mission2()
+  elif Button.RIGHT in brick.buttons():
+    print("Mission 6/7/9")
+    missions.Mission9_2()
+  elif Button.DOWN in brick.buttons():
+    while not (any(brick.buttons())):
+      wait(10)
+    if Button.UP in brick.buttons():
+      print("Mission 8 + Mission12_brown")
+      missions.Mission12.brown()
+      break
+    elif Button.RIGHT in brick.buttons():
+      print("Mission 12_blue")
+      missions.Mission12.blue()
+    elif Button.DOWN in brick.buttons():
+      print("Mission 12_white")
+      missions.Mission12.white()
+    elif Button.LEFT in brick.buttons():
+      print("Mission 12_red")
+      missions.Mission12.red()
+    elif Button.MIDDLE in brick.buttons():
+      break
+  elif Button.RIGHT in brick.buttons():
+    print("Mission 1")
+  elif Button.MIDDLE in brick.buttons():
+    selecting = False
+
+
 """
 # use brick buttons & a colour sensor to choose missions
 colour_sensor = ColorSensor(Port.S3)
