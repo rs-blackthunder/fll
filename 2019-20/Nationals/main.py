f45a3c383629ae8a43077e7285164b4bdedc06eb
TechnoLenzer <47294839+TechnoLenzer@users.noreply.github.com>
2020-02-19 10:36:00 +0000

    Update main.py

--- 2019-20/Nationals/main.py
+++ 2019-20/Nationals/main.py
@@ -12,6 +12,7 @@ import sys
 
 import movement, missions
 
+"""
 # test beeps
 brick.sound.beep(2000, 200, 50)
 
@@ -27,13 +28,14 @@ print(str(brick.battery.voltage()) + "mV")
 brick.display.clear()
 brick.display.text(str(brick.battery.current()) + "mA", (60, 50))
 brick.display.text(str(brick.battery.voltage()) + "mV")
+"""
 
 ########## TESTING ##########
 
 #movement.move(0,-400,1000)
 #movement.move(0,400,1000)
 #missions.Mission12.red()
-missions.Mission9_2()
+#missions.Mission9_2()
 #missions.Mission2()
 #missions.Mission2()
 #missions.Mission1()
@@ -80,10 +82,16 @@ while selecting:
   elif Button.MIDDLE in brick.buttons():
     selecting = False
 
+"""
 
+
+brick.display.text(str("Blue ^ Crane"), (60, 40))
+brick.display.text(str("< Red Brown >"), (60, 50))
+brick.display.text(str("White V Beams"), (60, 60))
 
 # use brick buttons & a colour sensor to choose missions
 colour_sensor = ColorSensor(Port.S3)
+
 while True:
   # wait until button is pressed
   while not (any(brick.buttons())):
@@ -91,10 +99,10 @@ while True:
 
   ######## attachments that do only one mission (missions 1, 6 & 7) ########
 
-  if colour_sensor.color() == Color.RED:
+  if colour_sensor.color() == Color.BLUE:
     # display mission numbers so we know what has been detected
     brick.display.text(str("Missions 1, 6, 7 & 8"), (60, 50))
-    print("Red")
+    print("Blue")
     # wait until button is pressed
     while not (any(brick.buttons())):
       wait(10)
@@ -107,7 +115,7 @@ while True:
       pass # mission 8
     elif Button.LEFT in brick.buttons():
       pass # mission 1
-    elif Button.MIDDLE in brick.buttons():  # in case colour sensor detects wrong colour
+    elif Button.CENTER in brick.buttons():  # in case colour sensor detects wrong colour
       break
 
   ##########################################################################
@@ -119,29 +127,40 @@ while True:
   # mission 12
   elif colour_sensor.color() == Color.GREEN:
     # display mission number so we know what has been detected
-    brick.display.text(str("Mission 12"))
+    print("Green")
     # clockwise is probable order (i.e. UP -> RIGHT -> DOWN -> LEFT)
     if Button.UP in brick.buttons():
+      brick.display.clear()
+      brick.display.text(str("Mission 12 Blue"), (60, 50))
       missions.Mission12.blue()
     elif Button.RIGHT in brick.buttons():
+      brick.display.clear()
+      brick.display.text(str("Mission 12 Brown/Elevator"), (60, 50))
       missions.Mission12.brown()
     elif Button.DOWN in brick.buttons():
+      brick.display.clear()
+      brick.display.text(str("Mission 12 White"), (60, 50))
       missions.Mission12.white()
     elif Button.LEFT in brick.buttons():
+      brick.display.clear()
+      brick.display.text(str("Mission 12 Red"), (60, 50))
       missions.Mission12.red()
-    elif Button.MIDDLE in brick.buttons():  # in case colour sensor detects wrong colour
+    elif Button.CENTER in brick.buttons():  # in case colour sensor detects wrong colour
       break
 
   # mission 2 & 9
-  elif colour_sensor.color() == Color.BLUE:
+  elif colour_sensor.color() == Color.RED:
     # display mission numbers so we know what has been detected
-    brick.display.text(str("Mission 2/9"))
-    if Button.LEFT in brick.buttons():
+    print("Red")
+    if Button.UP in brick.buttons():
+      brick.display.clear()
+      brick.display.text(str("Mission 2"), (60, 50))
       missions.Mission2()
-    elif Button.RIGHT in brick.buttons():
+    elif Button.DOWN in brick.buttons():
+      brick.display.clear()
+      brick.display.text(str("Mission 9"), (60, 50))
       missions.Mission9_2()
-    elif Button.MIDDLE in brick.buttons():  # in case colour sensor detects wrong colour
+    elif Button.CENTER in brick.buttons():  # in case colour sensor detects wrong colour
       break
 
   ##########################################################################
-  """
